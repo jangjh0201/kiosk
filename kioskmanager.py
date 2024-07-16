4d8a4aa53471c1381afffb4f181f893c3b142b33
jangjh0201 <jangjh0201@gmail.com>
2024-07-16 18:06:42 +0900

    kioskmanager

--- kioskmanager.py
+++ kioskmanager.py
@@ -1,16 +1,36 @@
 import os
 import sys
+import json
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import requests
 
-url = "http://127.0.0.1:8000/order"
-data = {
-    "ice_cream_name": "바닐라",
-    "topping_names": "초코볼, 시리얼",
-    "consumable_names": "스푼, 홀더",
-}
 
-response = requests.post(url, data=data)
-print(response.status_code)
+class DBClient:
+    def __init__(self, url):
+        self.url = url
+
+    def order_request(self):
+        try:
+            data = {"OR": {"icecream": "mint", "topping": "chocoball, cereal"}}
+            headers = {"Content-Type": "application/json"}
+            response = requests.post(self.url, headers=headers, data=json.dumps(data))
+            response.raise_for_status()  # 요청이 성공했는지 확인
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"주문 요청 처리 중 오류: {e}")
+            return None
+
+
+if __name__ == "__main__":
+    url = "http://172.30.1.5:8000/kiosk"
+    dbclient = DBClient(url)
+    response = dbclient.order_request()
+
+    if response:
+        print("Order ID:", response.get("orderId"))
+        print("Order IceCream:", response.get("icecream"))
+        print("Order Topping:", response.get("topping"))
+    else:
+        print("주문 요청에 실패했습니다.")
